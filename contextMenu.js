fb3a861a044e207bf120a129e57a90c19344e521
Erich Uher <ejuher@gmail.com>
2017-05-06 22:04:55 -0700

    stash links that are right-clicked

--- contextMenu.js
+++ contextMenu.js
@@ -1,18 +1,17 @@
-const parent = chrome.contextMenus.create({"title": "STASH"});
+const parent = chrome.contextMenus.create({"title": "STASH", "contexts":['link']});
 const tags = ["AUDIO", "VIDEO", "ARTICLE", "OTHER"];
 
 let menuItems = {};
 for (let i = 0; i < tags.length; i++) {
-  let id = chrome.contextMenus.create({"title": tags[i], "parentId": parent, "onclick": stash});
-  menuItems[id] = tags[i]
+  let id = chrome.contextMenus.create({"title": tags[i], "parentId": parent, "contexts":['link'], 
+                                       "onclick": stash});
+  menuItems[id] = tags[i];
 }
 
 function stash(info, tab) {
-  debugger
   const tag = menuItems[info.menuItemId];
-  const url = tab.url;
-  const title = tab.title;
-  stashURL(title, url, tag);
+  const url = info.linkUrl;
+  stashURL(null, url, tag);
 }
 
 function stashURL(title, url, tag) {
